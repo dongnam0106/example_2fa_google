ae08476dbcc8e461877c3625b709f0fabb68bc6d
Nguyen Dong Nam <dongnam.japan@gmail.com>
2023-07-11 09:07:16 +0700

    add security

---
+++ src/main/java/com/example/example_otp_with_google/config/CustomWebAuthenticationDetails.java
@@ -0,0 +1,19 @@
+package com.example.example_otp_with_google.config;
+
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class CustomWebAuthenticationDetails extends WebAuthenticationDetails {
+
+    private String verificationCode;
+
+    public CustomWebAuthenticationDetails(HttpServletRequest request) {
+        super(request);
+        verificationCode = request.getParameter("code");
+    }
+
+    public String getVerificationCode() {
+        return verificationCode;
+    }
+}
---
+++ src/main/java/com/example/example_otp_with_google/config/CustomWebAuthenticationDetailsSource.java
@@ -0,0 +1,18 @@
+package com.example.example_otp_with_google.config;
+
+import org.springframework.security.authentication.AuthenticationDetailsSource;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+@Component
+public class CustomWebAuthenticationDetailsSource implements
+        AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> {
+
+    @Override
+    public WebAuthenticationDetails buildDetails(HttpServletRequest context) {
+        return new CustomWebAuthenticationDetails(context);
+    }
+}
