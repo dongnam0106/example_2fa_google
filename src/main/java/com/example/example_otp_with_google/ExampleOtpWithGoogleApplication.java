99aa93b17def8b60a393c257461cd76e63e347aa
Nguyen Dong Nam <dongnam.japan@gmail.com>
2023-07-11 08:52:03 +0700

    init project

---
+++ src/main/java/com/example/example_otp_with_google/ExampleOtpWithGoogleApplication.java
@@ -0,0 +1,13 @@
+package com.example.example_otp_with_google;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleOtpWithGoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleOtpWithGoogleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/example_otp_with_google/ExampleOtpWithGoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.example_otp_with_google;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleOtpWithGoogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
